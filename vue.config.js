379a85b4941564003ad042bca5497673698c1eb4
Jani Maljanen <jani.maljanen@hotmail.com>
2020-02-12 00:58:34 +0200

    refactor

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {
+      name: 'Vaimo Barcode Reader',
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+    }
+  }
