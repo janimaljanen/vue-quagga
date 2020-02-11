ec4fea4eca39fd190bb975c84e3a9e7e2b4cfead
Jani Maljanen <jani.maljanen@hotmail.com>
2020-02-11 22:45:40 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Quagga/>
+  </div>
+</template>
+
+<script>
+import Quagga from './components/Quagga.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Quagga
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Quagga.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-quagga :onDetected="logIt" :readerSize="readerSize" :readerTypes="['ean_reader']"></v-quagga>
+    <div style="position: absolute;"> {{ code }} </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueQuagga from 'vue-quaggajs';
+
+// register component 'v-quagga'
+Vue.use(VueQuagga);
+
+export default {
+  name: 'VueBarcodeTest',
+  data () {
+    return {
+      readerSize: {
+        width: 640,
+        height: 480
+      },
+      detecteds: [],
+      code: ""
+    }
+  },
+  methods: {
+    logIt (data) {
+      console.log('detected', data)
+      this.code = data.codeResult.code
+    }
+
+  }
+}
+</script>
